4c63f8a27873dd02ac970621a733e691f30e3632
ChloeHJ <chloe.h.lee94@gmail.com>
2024-11-30 20:25:39 +0000

    debug

--- R/RunProjection.R
+++ R/RunProjection.R
@@ -239,7 +239,7 @@ RunProjection <- function(merge, coord_dt, knn_k = 100, n_harmony_dim = 35,  mul
   sp_counterparts <- sp_counterparts %>% left_join(coord_dt, by = c('spatial_id'))
 
   # add to multiome metadata
-  outs.metadata <- multiome@meta.data %>% rownames_to_column(var =  'multiome_id') %>%
+  outs.metadata <- merge@meta.data %>% rownames_to_column(var =  'multiome_id') %>%
     left_join(sp_counterparts, by = 'multiome_id')
 
   if(!is.null(plot_dir)){cairo_pdf(filename = paste0(plot_dir, '/3_spatial.location.of.multiome.pdf'), width = 7, height = 5)}
